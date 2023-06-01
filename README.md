83cff6f4229fdd387935cd3ee7000a477e9710f3
akinluyigithubm <akinluyiabe@gmail.com>
2023-06-01 02:49:35 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# This is a Maven Built Automation for app2
---
+++ src/main/java/com/appbuild/App.java
@@ -0,0 +1,13 @@
+package com.appbuild;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
